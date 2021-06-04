bda78313b1944d8ef03f868f23d449f9d91f52a6
krunal333 <53342447+krunal333@users.noreply.github.com>
2021-06-04 13:34:42 +0530

    first commit

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+module.exports = function(req, res, next){
+    const token = req.header('x-auth-token')
+
+    if (!token){
+        return res.status(401).json({ msg: 'No token, authorization failed' })
+    }
+
+    try {
+        const decode = jwt.verify(token, config.get('jwtSecret'))
+
+        req.user = decode.user
+        next()
+    } catch (err) {
+        req.status(401).json({ msg:'token is not valid' })
+    }
+}
